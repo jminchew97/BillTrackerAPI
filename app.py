2453fa92b6fc1d7be5aa0193b6a93e17f2fbdbbe
Josh Minchew <jminchew97@gmail.com>
2023-01-12 13:33:34 -0600

    created basic serializer for db data-> JSON

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, request
-from flask_smorest import Api
 import database
 import sqlite3
 import uuid
+import decimal  # use this instead
+from serializer import Serialize
 
 from datetime import date
 
-
 app = Flask(__name__)
+
+# Keys for bills serializer
+keys = ["bill_id", "bill_name", "amount", "due_date"]
+def foo(x: int) -> int:
+    return "yes"
+
 
 @app.post("/bill")
 def create_bill():
-
     """Create a new bill\n
     
     Expected JSON data:\n
@@ -23,53 +28,60 @@ def create_bill():
     
     """
     json_data = request.get_json()
-    bill_name = json_data['name']
-    bill_amount = json_data['amount']
-    due_date = json_data['due_date']
-    
+    bill_name: str = json_data['name']
+    bill_amount: decimal = decimal.Decimal(json_data['amount'])
+    due_date: int = int(json_data['due_date'])
+
     # create due date object
     current_date = date.today()
 
     # check if due date has passed or not
     try:
-        if current_date.day < int(due_date): # Due this month
+        if current_date.day <= int(due_date):  # Due this month
 
             # create due date object with due date being this month
-            due_date = date(current_date.year, current_date.month, int(due_date))
-            date_difference = due_date - current_date
+            due_date_obj: date = date(current_date.year, current_date.month, int(due_date))
+            date_difference = due_date_obj - current_date
 
-        elif current_date.day > int(due_date): # Due next month (add month to date object)
-            
+        elif current_date.day > int(due_date):  # Due next month (add month to date object)
+
             # add month to current month
-            if current_date.month == 12: # if current month is 12, set to january
-                due_date = date(current_date.year, 1, int(due_date))
-            else: # month is less than 12
-                due_date = date(current_date.year, current_date.month+1, int(due_date))
-            
+            if current_date.month == 12:  # if current month is 12, set to january
+                due_date_obj = date(current_date.year, 1, int(due_date))
+            else:  # current month is less than 12
+                due_date_obj = date(current_date.year, current_date.month + 1, int(due_date))
+
             # get difference in days before next bill is due
-            date_difference = due_date-current_date
+            date_difference = due_date_obj - current_date
+
     except ValueError:
-        return {"message":"Date you enters for month was not valid."}, 404
- 
+        return {"message": "Date you entered for month was not valid."}, 404
+
     # add bill to DB
-    database.create_bill_record(bill_name, bill_amount, due_date)
-    
-    return {"message":"Everything went well"}, 200
+    database.create_bill_record(bill_name, bill_amount, due_date_obj)
 
+    return {"message": "Everything went well"}, 200
 
+# Get all bills
 @app.get("/bill")
 def get_all_bills():
     all_items = database.show_all()
-    return all_items, 200
 
+    #debug
+    # print(f"keys:{keys} \n values:{all_items}")
+    # print("hello")
+    serialized = Serialize(keys, all_items).get()
+    return serialized, 200
+
+# Get specific bill ID
 @app.get("/bill/<string:id>")
 def get_bill(id):
     return database.select_record_by_id(id), 200
 
+# Delete specific bill by ID
 @app.delete("/bill/<string:id>")
 def delete_bill_by_id(id):
     database.delete_record_by_id(id)
-    return {"message":"Record has been deleted"}, 200
+    return {"message": "Record has been deleted"}, 200
 
-
-print("Hello")
+    # sort list by days left
