0eca560e680834a1e1e894f4ba524b5f481f6e88
Josh Minchew <jminchew97@gmail.com>
2023-01-24 16:41:57 -0600

    updated deserialize_row to dyamically assign field values based on the type given

--- data_handler.py
+++ data_handler.py
@@ -1,11 +1,19 @@
 import dataclasses
 from typing import TypeVar, Optional
 from dataclasses import dataclass, fields, replace
+from dataclasses_json import dataclass_json
 from decimal import Decimal
 from datetime import date, datetime
 import orjson
 import json as json_module
 
+import data_handler
+def cents_to_dollars(cents_amount: int) -> Decimal:
+    dollars = round(Decimal(cents_amount / 100), 2)
+    print(f"{cents_amount} -> {dollars}")
+    return Decimal(dollars)
+
+@dataclass_json
 @dataclass
 class Bill:
     """A finished bill object including all fields, has been already committed to database"""
@@ -14,6 +22,8 @@ class Bill:
     amount: Decimal
     due_date: datetime.date
 
+
+@dataclass_json
 @dataclass
 class BillCreate:
     """A bill data type with no id because it has not been generated and stored in the database yet"""
@@ -21,9 +31,10 @@ class BillCreate:
     amount: Decimal
     due_date: datetime.date
 
+
+@dataclass_json
 @dataclass
 class BillEdit:
-
     id: int
     name: Optional[str] = None
     amount: Optional[Decimal] = None
@@ -31,6 +42,7 @@ class BillEdit:
 
 
 T = TypeVar('T')
+
 
 # test
 
@@ -45,17 +57,46 @@ def default(obj: type[T]):
 def deserialize_row(typ: type[T], row: list[tuple[object]]) -> T:
     """deserialize row from SQLITE"""
 
-    if len(row) == 1:
+    # checks to see if there's single row in the returned list i.e [[1,"a",12.3]],
+    # rather than [[1,"a",12.3], [1,"a",12.3]]
+    if len(row) == 1:  # if so, selects that row to iterate through
         row = row[0]
 
-    # TODO figure out error
-    date = datetime.strptime(row[3], "%Y-%m-%d").date()
+    # List to hold values after casted to proper type
+    deserialized_values = []
 
-    #               id      name    amount                  date
-    new_bill = typ(row[0], row[1], cents_to_dollars(row[2]), date)  # TODO change string to date, using string to test
+    # make sure values are equal to expected fields
+    if len(fields(typ)) != len(row):
+        return Exception(f"{len(fields(typ))} fields were expected, recieved {len(row)}")
 
-    # use orjson to serialize to json
-    return new_bill
+    # iterate through typ field with corresponding value
+    for field, value in zip(fields(typ), row):
+
+        field_type = typ.__annotations__[field.name]
+        # check what type current field is expecting, then cast corresponding value to that type
+        # then append to deserialized_values
+        if field_type == int:
+            deserialized_values.append(int(value))
+
+        elif field_type == str:
+            deserialized_values.append(str(value))
+
+        elif field_type == datetime.date:
+            deserialized_values.append(datetime.strptime(value, "%Y-%m-%d").date())
+
+        # converts to decimal while also converting from_cents_to_dollars, only if this is an "amount" of money
+        elif field_type == Decimal and field.name == "amount":
+            deserialized_values.append(cents_to_dollars(value))
+
+        elif field_type == Decimal:  # not an amount of money
+            deserialized_values.append(Decimal(value))
+
+    # create new type, unpack dezerialized_values
+    new_typ = typ(*deserialized_values)
+
+    return new_typ
+
+
 
 
 def deserialize_rows(typ: type[T], rows: list[tuple[object]]) -> list[T]:
@@ -71,11 +112,13 @@ def serialize_to_json(typ: dataclasses.dataclass()) -> dict:
 def deserialize_json_on_post(typ: type[T], json_data: dict) -> T:
     if typ == BillCreate:
         print(f"Type of argument before error : {type(round(Decimal(json_data['amount']), 2))}")
-        new_bill = BillCreate(json_data["name"], round(Decimal(json_data['amount']), 2), str_to_date_obj(json_data["due_date"]))
+        new_bill = BillCreate(json_data["name"], round(Decimal(json_data['amount']), 2),
+                              str_to_date_obj(json_data["due_date"]))
 
     else:
         raise Exception("Dont know how to deserialize this type of object. You entered type {type(typ}.")
     return new_bill
+
 
 def deserialize_json_on_put(typ: type[T], jdata: dict) -> T:
     new_bill = BillEdit(1)
@@ -83,6 +126,7 @@ def deserialize_json_on_put(typ: type[T], jdata: dict) -> T:
     edited_bill = replace(new_bill, **jdata)
 
     return edited_bill
+
 
 def dollars_to_cents(dollar_amount: Decimal) -> int:
     if not isinstance(dollar_amount, Decimal):
@@ -93,10 +137,7 @@ def dollars_to_cents(dollar_amount: Decimal) -> int:
     return cents
 
 
-def cents_to_dollars(cents_amount: int) -> Decimal:
-    dollars = round(Decimal(cents_amount / 100), 2)
-    print(f"{cents_amount} -> {dollars}")
-    return Decimal(dollars)
+
 
 
 def str_to_date_obj(due_date: int) -> date:
@@ -122,4 +163,3 @@ def str_to_date_obj(due_date: int) -> date:
             return due_date_obj
     except ValueError:
         return {"message": "Date you entered for month was not valid."}, 404
-
