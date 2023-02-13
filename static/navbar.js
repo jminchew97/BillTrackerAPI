625ae77979e97c46c4f442802b501d2e1b485813
Josh Minchew <jminchew97@gmail.com>
2023-02-13 12:41:28 -0600

    added create bill function on dashboard; can now delete a row; removed create/edit pages

--- static/navbar.js
+++ static/navbar.js
@@ -5,12 +5,8 @@ document.write('<nav class="navbar navbar-expand-lg navbar-dark bg-dark">\
   </button>\
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">\
     <div class="navbar-nav">\
-      <a class="nav-item nav-link active" href="http://127.0.0.1:5000/">Home <span class="sr-only">(current)</span></a>\
+      <a class="nav-item nav-link" href="http://127.0.0.1:5000/">Home <span class="sr-only">(current)</span></a>\
       <a class="nav-item nav-link" href="http://127.0.0.1:5000/dashboard">Dashboard</a>\
-      \<!-- ask cary if this is the right way of doing this (below)-->\
-      <a class="nav-item nav-link" href="http://127.0.0.1:5000/create">Create</a>\
-      <a class="nav-item nav-link" href="http://127.0.0.1:5000/edit">Edit</a>\
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>\
     </div>\
   </div>\
 </nav>\
